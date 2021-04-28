0789647757b64d52fd5ffbcb64b9834e58887cc2
guoyu <xxx@xxx>
2021-04-28 08:28:20 +0800

    之字打印

--- binaryTreeNode/levelOrder.h
+++ binaryTreeNode/levelOrder.h
@@ -4,6 +4,7 @@
 #include "vector"
 #include "treeDepth.h"
 #include "queue"
+#include "stack"
 #ifndef ALGORITHM_LEVELORDER_H
 #define ALGORITHM_LEVELORDER_H
 
@@ -63,6 +64,7 @@ vector<int> levelOrder(TreeNode* root) {
 //  [20,9],
 //  [15,7]
 //]
+//之字形打印二叉树第一种解法
 vector<vector<int>> levelOrder(TreeNode* root) {
     vector<vector<int>> res;
     if(root==nullptr) return res;
@@ -84,6 +86,46 @@ vector<vector<int>> levelOrder(TreeNode* root) {
             reverse(vc.begin(),vc.end());
         }
         if(vc.size()>0) res.push_back(vc);
+    }
+    return res;
+}
+
+//之字形打印二叉树第二种解法
+//使用两个栈，在打印一个栈的节点时，把它的子节点保存在另一个栈里。
+vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> res;
+    if(root==nullptr) return res;
+    stack<TreeNode*> levels[2];
+    int current=0;
+    int next=1;
+
+    vector<int> tmp;
+    levels[current].push(root);
+    while(!levels[0].empty()||!levels[1].empty()){
+        TreeNode* node=levels[current].top();
+        levels[current].pop();
+        tmp.push_back(node->val);
+        if(current==0){
+            if(node->left!=nullptr){
+                levels[next].push(node->left);
+            }
+            if(node->right!=nullptr){
+                levels[next].push(node->right);
+            }
+        }else{
+            if(node->right!=nullptr){
+                levels[next].push(node->right);
+            }
+            if(node->left!=nullptr){
+                levels[next].push(node->left);
+            }
+        }
+        if(levels[current].empty()){
+            res.push_back(tmp);
+            tmp.clear();
+            current=1-current;
+            next=1-next;
+        }
     }
     return res;
 }
